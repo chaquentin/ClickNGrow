719bbafe5890169bb3a97cc1b80e133bfac5efac
Yom <tom.laiolo@epitech.eu>
2023-02-04 15:33:28 +0100

    [ADD] Random pub launched with P

---
+++ src/Pub.cpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2023
+** ClickNGrow
+** File description:
+** Pub
+*/
+
+#include <iostream>
+#include <vector>
+
+void Pub()
+{
+    std::vector<std::string> videos = {"assets/pubs/axe.mp4", "assets/pubs/carrefour.mp4"};
+    std::string command = "vlc " + videos[rand() % videos.size()] + " --play-and-exit";
+    int result = std::system(command.c_str());
+}
