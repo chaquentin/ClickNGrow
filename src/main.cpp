f03a77485db24a52c1023c68f3cf0a63a1952e36
Yom <tom.laiolo@epitech.eu>
2023-02-04 18:34:25 +0100

    [ADD] pub tuneup

--- src/main.cpp
+++ src/main.cpp
@@ -45,6 +45,10 @@ int main(void)
     float money = 0;
     float deltaTime = 0;
     sf::Clock clock;
+    sf::Text money_text;
+    sf::Font font;
+    font.loadFromFile("assets/fonts/ClickNGrow.ttf");
+    money_text.setFont(font);
 
     while (window.isOpen()) {
         sf::Event event;
@@ -53,12 +57,19 @@ int main(void)
                 window.close();
             if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::P)
                 Pub();
+            if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left)
+                if (event.mouseButton.x >= 0 && event.mouseButton.x <= 1280 && event.mouseButton.y >= 0 && event.mouseButton.y <= 1080)
+                    money += 1;
         }
         deltaTime += clock.getElapsedTime().asSeconds();
         update(money, deltaTime);
         window.clear();
         displayScene(scene);
         display();
+        money_text.setString(std::to_string((int)money) + "*");
+        money_text.setFillColor(sf::Color::White);
+        money_text.setPosition(0, 0);
+        window.draw(money_text);
         window.display();
         if (deltaTime > 1)
             deltaTime = 0;
