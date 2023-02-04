742ede8db50bc28218ca8b9b470a1b9fb6737d08
Yom <tom.laiolo@epitech.eu>
2023-02-04 18:34:25 +0100

    [ADD] pub tuneup

--- src/Pub.cpp
+++ src/Pub.cpp
@@ -10,7 +10,7 @@
 
 void Pub()
 {
-    std::vector<std::string> videos = {"assets/pubs/axe.mp4", "assets/pubs/carrefour.mp4"};
+    std::vector<std::string> videos = {"assets/pubs/axe.mp4", "assets/pubs/carrefour.mp4", "assets/pubs/tuneup.mp4"};
     std::string command = "vlc " + videos[rand() % videos.size()] + " --play-and-exit";
     int result = std::system(command.c_str());
 }
