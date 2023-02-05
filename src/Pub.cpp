b4defdd45a104f6410da7ac2545f088db1c457f3
Yom <tom.laiolo@epitech.eu>
2023-02-05 03:22:41 +0100

    [FIX] warnings and add pubs to earn money with P

--- src/Pub.cpp
+++ src/Pub.cpp
@@ -7,10 +7,27 @@
 
 #include <iostream>
 #include <vector>
+#include "Hud.hpp"
+#include "GameObject.hpp"
+
+using namespace clickNGrow;
 
 void Pub()
 {
     std::vector<std::string> videos = {"assets/pubs/axe.mp4", "assets/pubs/carrefour.mp4", "assets/pubs/tuneup.mp4", "assets/pubs/crazytyger.mp4", "assets/pubs/zalando.mp4"};
     std::string command = "vlc " + videos[rand() % videos.size()] + " --play-and-exit";
-    int result = std::system(command.c_str());
+    std::system(command.c_str());
+}
+
+void PubForMoney(clickNGrow::Hud *hud, std::vector<clickNGrow::GameObject *> gameObjects)
+{
+    double total = 0;
+    Pub();
+    for (auto &object : gameObjects) {
+        total += object->getMoney() * object->getAmount();
+    }
+    total *= 60;
+    if (total < 50)
+        total = 80;
+    *hud += total;
 }
